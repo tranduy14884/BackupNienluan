8e778de8da7dd4b83e7944688ec4d1bfc31705c5
Duy Tran <tranduy14884@gmail.com>
2021-04-13 22:50:15 +0700

    update fullName field

--- src/Components/FormControl/InputField/index.jsx
+++ src/Components/FormControl/InputField/index.jsx
@@ -1,43 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { TextField } from "@material-ui/core";
-import { Controller } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message";
 
 InputField.propTypes = {
-  form: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired,
-  label: PropTypes.string,
-  disable: PropTypes.bool,
+  // register: PropTypes.string.isRequired,
+  // name: PropTypes.string.isRequired,
+  // disable: PropTypes.bool,
+  // errors : PropTypes.object.isRequired,
 };
 
 function InputField(props) {
-  const { form, name, label, disable } = props;
-  const { controls, formState } = form;
-  const hasErr = formState.touchedFields[name] && formState.errors[name];
-  // console.log(formState);
-  // console.log(formState.errors[name], formState.touchedFields[name]);
+  const { register, name, disable , errors} = props;
   return (
     <>
-    <Controller
-      name={name}
-      control={form.control}
-      render={({
-        field: { onChange, onBlur, value, name, ref },
-        fieldState: { invalid, isTouched, isDirty, error },
-        formState,
-      }) => <TextField  />}
-      fullWidth
-     
-      disable={disable}
-      label={label}
-
-     
-      //helperText={formState.errors[name]?.message}
-    >
-        
-    </Controller>
-    <p>{formState.errors[name]?.message}</p>
+      <InputField  
+        {...register({name})} 
+        defaultValue="" 
+      />
+      <p>{errors.name?.message}</p>
     </>
   );
 }
--- src/Features/Auth/Components/RegisterForm/index.jsx
+++ src/Features/Auth/Components/RegisterForm/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-// import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { TextField } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import InputField from '../../../../Components/FormControl/InputField';
-import { useForm } from 'react-hook-form';
+import './style.css';
 
 RegisterForm.propTypes = {
   onSubmit: PropTypes.func,
@@ -15,28 +16,24 @@ function RegisterForm(props) {
     fullName: yup.string().required('Please enter data'),
 
   });
-  
-
-  const form = useForm({
-    defaultValues: {
-      fullName: '',
-    },
+;
+  const { register, formState: { errors }, handleSubmit } = useForm({
     resolver: yupResolver(schema),
+    defaultValue : {
+      fullName : '',
+    }
   });
-
-  const handleSubmit = (value) => {
-    // const {onSubmit} = props;
-    // if(onSubmit)
-    // {
-    //     onSubmit(value);
-    // }
-    // form.reset();
-    console.log(value);
-  };
+  const onSubmit = data => console.log(data);
   return (
-    <form onSubmit={form.handleSubmit(handleSubmit)}>
-      <InputField name="fullName" form={form} label="todo" />
     
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <p className="field-title">Ho va ten</p>
+      <TextField {...register("fullName")}/>
+      <p className="message">{errors.fullName?.message}</p>
+
+
+      {/* <InputField register={register} errors = {errors} name="fullName"></InputField> */}
+      <button type="submit">Create an account</button>
     </form>
   );
 }
